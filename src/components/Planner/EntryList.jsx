d4fc9f2a01a379f19f2c41b44c8c409d126fb67d
m-ghiringhelli <mghiring@gmail.com>
2022-05-16 17:20:47 -0700

    entrylist uses context to display entries stored in context

--- src/components/Planner/EntryList.jsx
+++ src/components/Planner/EntryList.jsx
@@ -1,16 +1,11 @@
+import { useContext } from 'react';
+import { PlannerContext } from '../../context/PlannerContext';
 import Entry from './EntryItem';
 import styles from './EntryList.css';
 
 export default function Entries() {
   // TODO: Replace with the entries stored in PlannerContext
-  const entries = [
-    {
-      id: 0,
-      title: 'Replace me!',
-      content: '`content` should only be visible on the detail view',
-      date: new Date()
-    },
-  ];
+  const entries = useContext(PlannerContext).entries;
 
   return (
     <ul className={styles.list}>
